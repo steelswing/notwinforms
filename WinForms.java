a8776989f7ef5c4840c353a319e97d2bfab428b4
SteelSwing <53971838+steelswing@users.noreply.github.com>
2021-12-09 14:18:17 +1000

    Add files via upload

--- WinForms.java
+++ WinForms.java
@@ -5,11 +5,15 @@
 package test;
 
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
+import java.awt.image.WritableRaster;
 import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 /**
@@ -18,58 +22,59 @@ import javax.swing.JPanel;
  */
 public class WinForms extends JPanel {
 
+    private static final long serialVersionUID = 1L;
+
     private JFrame frame;
     private BufferedImage img;
 
     public WinForms() throws Exception {
         { // init panel
             BufferedImage in = ImageIO.read(getClass().getResourceAsStream("/test/PNG_transparency_demonstration_1.png")); // Грузим пикчу
-            img = in;
+            FastRGB data = new FastRGB(img = in);
             {
                 JButton button = new JButton("Click me");
-                button.addActionListener((e) -> {
+                button.addActionListener((ActionEvent e) -> {
                     long lastTime = System.nanoTime();
-                    for (int i = 0; i < 1000; i++) {
-                        {
-                            img = new BufferedImage(in.getWidth(), in.getHeight(), BufferedImage.TYPE_INT_ARGB);
-                            FastRGB data = new FastRGB(in);
+                    {
+                        img = new BufferedImage(in.getWidth(), in.getHeight(), BufferedImage.TYPE_INT_ARGB);
+                        final int width = img.getWidth();
+                        final int height = img.getHeight();
 
-                            for (int x = 0; x < img.getWidth(); x++) {
-                                for (int y = 0; y < img.getHeight(); y++) {
-                                    short[] pixel = data.getRGB(x, y);
+                        final int[] pixels = new int[width * height * 4];
+                        for (int i = 0; i < 1000; i++) {
+                            for (int x = 0; x < width; x++) {
+                                for (int y = 0; y < height; y++) {
+                                    int pixel = data.getRGB(x, y);
+                                    int pos = (y * 4 * width) + (x * 4);
 
-                                    int r = 255;//pixel[0];
-                                    int g = pixel[1];
-                                    int b = pixel[2];
-                                    int a = pixel[3];
-
-                                    int rgb =
-                                            ((a & 0xFF) << 24) |
-                                            ((r & 0xFF) << 16) |
-                                            ((g & 0xFF) << 8) |
-                                            ((b & 0xFF) << 0);
-                                    img.setRGB(x, y, rgb);
+                                    pixels[pos++] = pixel >> 20; // Тута должно быть 24, но для теста написаль 
+                                    pixels[pos++] = pixel >> 16;
+                                    pixels[pos++] = pixel >> 8;
+                                    pixels[pos++] = pixel >> 0;
                                 }
                             }
                         }
+                        WritableRaster raster = (WritableRaster) img.getData();
+                        raster.setPixels(0, 0, width, height, pixels);
+                        img.setData(raster);
                     }
+
                     long curTime = System.nanoTime();
                     long da = (curTime - lastTime) / 1_000_000;
-                    System.out.println("Time: " + da + " ms");
-
-                    try {
-                        // Если нужно записать пикчу
-                        ImageIO.write(img, "PNG", new java.io.File("output.png"));
-                    } catch (Exception otvalException) {
-                        otvalException.printStackTrace();
-                    }
-
                     repaint(); // Перерисовка канваса
+                    System.out.println("Time: " + da + " ms");
+                    JOptionPane.showMessageDialog(null, "Time: " + da + " ms", "WinForms", JOptionPane.INFORMATION_MESSAGE);
                 });
                 add(button);
             }
+            setBorder(BorderFactory.createTitledBorder("Hello World"));
         }
         frame = new JFrame(getClass().getName());
+
+        try {
+            frame.setIconImage(ImageIO.read(getClass().getResourceAsStream("/test/icon.png")));
+        } catch (Exception e) {
+        }
         frame.setSize(800, 600);
         frame.add(this); // Добавить это говно на фрейм
         frame.setLocationRelativeTo(null);
@@ -100,32 +105,45 @@ public class WinForms extends JPanel {
  */
 class FastRGB {
 
-    public int width;
-    public int height;
-    private boolean hasAlphaChannel;
-    private int pixelLength;
-    private byte[] pixels;
+    public final int width;
+    public final int height;
+    private final boolean hasAlphaChannel;
+    private final int pixelLength;
+    private final byte[] pixels;
+
+    private final int[][] readedPixels;
 
     public FastRGB(BufferedImage image) {
         pixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
         width = image.getWidth();
         height = image.getHeight();
         hasAlphaChannel = image.getAlphaRaster() != null;
-        pixelLength = 3;
-        if (hasAlphaChannel) {
-            pixelLength = 4;
+        pixelLength = hasAlphaChannel ? 4 : 3;
+        readedPixels = new int[width][height];
+
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                int pos = (y * pixelLength * width) + (x * pixelLength);
+                byte rgb[] = new byte[4];
+                if (hasAlphaChannel) {
+                    rgb[3] = (byte) (pixels[pos++] & 0xFF); // Alpha
+                }
+                rgb[2] = (byte) (pixels[pos++] & 0xFF); // Blue
+                rgb[1] = (byte) (pixels[pos++] & 0xFF); // Green
+                rgb[0] = (byte) (pixels[pos++] & 0xFF); // Red
+
+                int v =
+                        ((rgb[0] & 0xFF) << 24) |
+                        ((rgb[1] & 0xFF) << 16) |
+                        ((rgb[2] & 0xFF) << 8) |
+                        ((rgb[3] & 0xFF) << 0);
+
+                readedPixels[x][y] = v;
+            }
         }
     }
 
-    public short[] getRGB(int x, int y) {
-        int pos = (y * pixelLength * width) + (x * pixelLength);
-        short rgb[] = new short[4];
-        if (hasAlphaChannel) {
-            rgb[3] = (short) (pixels[pos++] & 0xFF); // Alpha
-        }
-        rgb[2] = (short) (pixels[pos++] & 0xFF); // Blue
-        rgb[1] = (short) (pixels[pos++] & 0xFF); // Green
-        rgb[0] = (short) (pixels[pos++] & 0xFF); // Red
-        return rgb;
+    public int getRGB(int x, int y) {
+        return readedPixels[x][y];
     }
 }
